52da79e967d92036b02dcc165cfb4705c2a44ae0
Louis Marceron <louis.marceron@protonmail.com>
2024-03-02 16:39:42 +0100

    feat: add login + register + forgot password + edit profile

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Banking-App
-Application to monitor your finances
+# Firebase Auth with FlutterFire UI
+
+This code supports the [Firebase Auth with FlutterFire UI][] codelab.
+The [`/complete`](./complete) directory is the state that the 
+app should be in at the end of the codelab. 
+
+[Firebase Auth with FlutterFire UI]: https://firebase.google.com/codelabs/firebase-auth-in-flutter-apps
---
+++ lib/app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'auth_gate.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      themeMode: ThemeMode.system,
+      home: const AuthGate(),
+    );
+  }
+}
