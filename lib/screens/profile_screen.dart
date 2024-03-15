ea443006e6e3c229063a8374a7d3a9aa5b45f103
NathanaelCostes <nathanaelcostes7@gmail.com>
2024-03-15 14:23:38 +0100

    feat(profile_screen) : reset password

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -7,7 +7,11 @@ class ProfileScreen extends StatelessWidget {
 
   void _deleteAccount(BuildContext context) async {
     print('Deleting account');
+
     final user = FirebaseAuth.instance.currentUser;
+
+    //await user?.reauthenticateWithCredential(credential);
+
 
     final confirmed = await showConfirmationDialog(
       context,
@@ -31,8 +35,71 @@ class ProfileScreen extends StatelessWidget {
     Navigator.of(context).pop();
   }
 
+  void resetPassword(BuildContext context, String? currentPassword) async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null && currentPassword != null && user.email != null) {
+
+      AuthCredential credential =
+      EmailAuthProvider.credential(email: user.email!, password: currentPassword);
+
+      try {
+        // Reauthenticate the user with the credential
+        await user.reauthenticateWithCredential(credential);
+
+        // If reauthentication is successful, send the password reset email
+        await FirebaseAuth.instance.sendPasswordResetEmail(email: user.email!);
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Password reset email sent to ${user.email}'),
+          ),
+        );
+      } catch (e) {
+        print('Failed to reauthenticate: $e');
+        // Handle reauthentication errors accordingly
+      }
+    } else {
+      // Handle the case where currentPassword is null
+      print('Current password is null');
+    }
+  }
+
+
+
+  void changeEmail(BuildContext context, String? currentPassword) async {
+    // Implement the logic to change email
+    // For example:
+    // FirebaseAuth.instance.currentUser?.updateEmail(newEmail);
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null && currentPassword != null && user.email != null) {
+
+      AuthCredential credential =
+      EmailAuthProvider.credential(email: user.email!, password: currentPassword);
+
+      try {
+        // Reauthenticate the user with the credential
+        await user.reauthenticateWithCredential(credential);
+
+        // If reauthentication is successful, update the email
+        //TODO TextFIeld pour nouveau email
+        await user.verifyBeforeUpdateEmail('TODO: new email address');
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Email updated to ${user.email}'),
+          ),
+        );
+      } catch (e) {
+        print('Failed to reauthenticate: $e');
+        // Handle reauthentication errors accordingly
+      }
+
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser;
     return Scaffold(
       appBar: AppBar(
         title: const Text('User Screen'),
@@ -46,20 +113,57 @@ class ProfileScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('FDP'),
-            OutlinedButton(
-              onPressed: () {
-                _deleteAccount(context);
-              },
-              child: const Text('Delete Account'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            padding: EdgeInsets.all(20.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(25),
+              color: Colors.grey[100],
             ),
-            // Add more widgets specific to the user screen
-          ],
-        ),
+            child: Column(
+              children: [
+                Icon(Icons.account_circle, size: 100),
+                SizedBox(height: 10),
+                if (user != null) ...[
+                  Text(
+                    'Email: ${user.email}',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ],
+              ],
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      changeEmail(context, "TODO: current password");
+                    },
+                    child: const Text("Change Email"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      resetPassword(context, "TODO: current password");
+                    },
+                    child: const Text('Reset Password'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      _deleteAccount(context);
+                    },
+                    child: const Text('Delete Account', style: TextStyle(color: Colors.red)),
+                  ),
+                  // Add more widgets specific to the user screen
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
