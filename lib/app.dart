c3a4cb7d90d7ed9526bdcf6921929936a131c731
Louis Marceron <louis.marceron@protonmail.com>
2024-04-01 22:07:21 +0200

    refactor: simplify colorscheme

--- lib/app.dart
+++ lib/app.dart
@@ -1,19 +1,29 @@
 import 'package:flutter/material.dart';
 
 import 'auth_gate.dart';
-import 'theme.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    final materialTheme = MaterialTheme(Theme.of(context).textTheme);
+    const Color seedColor = Color(0xFF047080);
 
     return MaterialApp(
-      theme: materialTheme.light(),
-      darkTheme: materialTheme.dark(),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: seedColor,
+        ),
+        useMaterial3: true,
+      ),
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: seedColor,
+          brightness: Brightness.dark,
+        ),
+        useMaterial3: true,
+      ),
       themeMode: ThemeMode.system,
+      title: 'Limpide',
       home: const AuthGate(),
     );
   }
